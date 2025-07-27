8370104d877d8a10553685f522952d7bc07a0e89
Zer0Access <jamesw6734@outlook.com>
2025-07-27 16:11:04 +0100

    Initial Commit

---
+++ src/main/kotlin/org/zero/test_Plugin/Test_Plugin.kt
@@ -0,0 +1,14 @@
+package org.zero.test_Plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Test_Plugin : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
