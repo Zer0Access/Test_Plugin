a015ae3014986a0675863a835829d5f557936c26
Zer0Access <jamesw6734@outlook.com>
2025-07-27 19:10:33 +0100

    now slows after 5s

--- src/main/kotlin/org/zero/test_Plugin/Test_Plugin.kt
+++ src/main/kotlin/org/zero/test_Plugin/Test_Plugin.kt
@@ -8,12 +8,13 @@ import org.bukkit.event.entity.EntityDamageEvent
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.entity.EntityResurrectEvent
+import kotlin.concurrent.thread
 
 class Test_Plugin : JavaPlugin(), Listener {
 
     override fun onEnable() {
         // Plugin startup logic
-        server.pluginManager.registerEvents(this,this)
+        server.pluginManager.registerEvents(this, this)
     }
 
     @EventHandler
@@ -32,9 +33,13 @@ class Test_Plugin : JavaPlugin(), Listener {
 
     @EventHandler
     fun onUseTotem(event: EntityResurrectEvent) {
-    if (event.entity is Player) {
-        val player = event.entity as Player
-        player.walkSpeed = 0.2f // Reset walk speed
-    }
+        if (event.entity is Player) {
+            val player = event.entity as Player
+            player.walkSpeed = 0.4f // Doubles walk speed
+            thread(start = true) {
+                Thread.sleep(5000) // Wait for 5 seconds
+                player.walkSpeed = 0.2f // Resets walk speed to normal
+            }
+        }
     }
 }
